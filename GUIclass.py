23d78300fee73c97b3f272dcd4773de8c82d0a97
KarenaJGarcia <45367166+KarenaJGarcia@users.noreply.github.com>
2018-11-29 10:51:18 -0500

    Add files via upload

---
+++ GUIclass.py
@@ -0,0 +1,94 @@
+from tkinter import *
+
+class Application(Frame):
+    """A GUI appllication."""
+
+    def __init__(self,master):
+        """Initialize the Frame"""
+        Frame.__init__(self,master)
+        self.grid()
+        self.create_widgets()
+        
+
+    def create_widgets(self):
+        """Create 4 buttons"""
+
+        #label 1
+        self.label = Label(self, text = "Welcome to Route Runner!")
+        self.label.grid()
+
+        #label 2
+        self.label2 = Label(self, text = "What would you like to do?")
+        self.label2.grid()
+        
+        #button 1
+        self.button1 = Button(self,text = "Choose a Route",command=self.create_window)
+        self.button1.grid()
+
+        #button 2
+        self.button2 = Button(self)
+        self.button2.grid()
+        self.button2.configure(text = "Add a Route")
+
+        #button 3
+        self.button3 = Button(self)
+        self.button3.grid()
+        self.button3.configure(text = "View all Routes", command = self.open_allroutes)
+
+        #button 4
+        self.button4 = Button(self)
+        self.button4.grid()
+        self.button4.configure(text = "Favorites",command=self.open_favorites)
+        
+
+        #button 5
+        self.button5 = Button(self)
+        self.button5.grid()
+        self.button5.configure(text = "Quit",command= self.client_exit)
+
+    def client_exit(self):
+        exit()
+
+    def open_favorites(self):
+        file = open("favoriteroutes.txt","r")
+        print(file.read())
+        file.close
+
+    def open_allroutes(self):
+        file = open("routefile.txt","r")
+        print(file.read())
+        file.close
+        file = open("favoriteroutes.txt","r")
+        print(file.read())
+        file.close()
+
+    def create_window(self):
+        
+        #create window
+        root = Tk()
+        root.title("Route Runner: Choose a Route")
+        root.geometry("300x350")
+        
+
+        #entry box1
+        mileage_entry = Entry()
+        mileage_entry.grid()
+
+        app = Frame(root)
+        app.grid()
+        root.mainloop()
+
+        
+        
+
+root = Tk()
+root.title("Route Runner")
+root.geometry("300x350")
+
+app = Application(root)
+root.mainloop()
+
+
+
+
+        
---
+++ prototypingBrainstorm.py
@@ -0,0 +1,64 @@
+option = input("What would you like to do? (CHOOSE, ADD, FAVORITES, VIEW, QUIT: ").upper()
+route_list = []
+routes = {}
+
+while option != "QUIT":
+
+    if option == "ADD":
+        routes['name'] = input("Name: ")
+        routes['miles'] = input("Length in Mileage: ")
+        routes['time'] = input("Time in Minutes: ")
+        routes['terrain']=input("Terrain (Grass, Gravel, Asphalt, Pavement): ")
+        routes['favorite']=input("Is this a Favorite Route?(Y/N): ").upper()
+
+        if routes['favorite'] == "Y":
+            file = open("favoriteroutes.txt","a")
+            file.write(routes['name']+" ")
+            file.write(routes['miles']+" ")
+            file.write(routes['time']+" ")
+            file.write(routes['terrain']+" ")
+            file.write("\n")
+
+        else:
+
+            file = open("routefile.txt","a")
+            file.write(routes['name']+" ")
+            file.write(routes['miles']+" ")
+            file.write(routes['time']+" ")
+            file.write(routes['terrain']+" ")
+            file.write("\n")
+
+
+    elif option == "VIEW":
+        file = open("routefile.txt","r")
+        print(file.read())
+        file.close
+        file = open("favorite.txt","r")
+        print(file.read())
+        file.close()
+
+    elif option == "FAVORITES":
+        file = open("favoriteroutes.txt","r")
+        print(file.read())
+
+    else: #instead of writing just 6 put six miles...when adding it to 6 miles  
+        mileage = input("How far would you like to run today in miles?: ")
+        time = input("How long would you like to run in minutes?: ")
+        file = open("routefile.txt","r")
+        contents = file.readlines()
+        file = open("favoriteroutes.txt","r")
+        fave_contents = file.readlines()
+        for elements in contents:
+            if mileage in elements or time in elements:
+                print(elements)
+        for elements in fave_contents or time in elements:
+            if mileage in elements:
+                print(elements)
+        
+        
+    option = input("What would you like to do? (CHOOSE, ADD, REMOVE, FAVORITES, VIEW, QUIT: ").upper()
+    
+    
+    
+    
+    
